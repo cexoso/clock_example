372ff4fde29dc98cf97c36439a86f0a2c364193f
cesoxiong <cesoxiong@tencent.com>
2019-08-01 14:53:54 +0800

    完整

--- src/clock.js
+++ src/clock.js
@@ -3,10 +3,17 @@ import Tick from "./tick"
 import cls from './clock.module.css'
 
 export default class Clock extends React.PureComponent {
+    state = {
+        wisePosition: 0
+    }
+    onClickBigger = (key) => {
+        this.setState({ wisePosition: key });
+    }
     render() {
+        const { wisePosition } = this.state
         return <div className={cls.container}>
-            <Tick />
-            <div className={cls.clockWise} />
+            <Tick onClickBigger={this.onClickBigger} wisePosition={wisePosition} />
+            <div className={cls.clockWise} style={{transform: `rotate(${360 / 72 * wisePosition - 90}deg)`}} />
             <div className={cls.center} />
         </div>
     }
--- src/tick.js
+++ src/tick.js
@@ -2,10 +2,14 @@ import React from 'react';
 import cls from './tick.module.css'
 // 用于画刻度
 export default class Tick extends React.PureComponent {
+    onClick = (key) => () => {
+        this.props.onClickBigger(key)
+    }
     render() {
         // 72 个小白点
         const size = 72;
         const pointets = Array.from({ length: size })
+        const { wisePosition } = this.props
         return <>
             {pointets.map((item, key) => {
                 // 中心偏移
@@ -13,9 +17,10 @@ export default class Tick extends React.PureComponent {
                 // 每个点相对于中心的弧度 以 Y 轴为 0PI
                 const radian = key / 36 * Math.PI + Math.PI
                 const bigger = key % 9 === 0
-                return <div className={`${cls.pointer} ${bigger ? cls.big : cls.small}`} key={key} style={{
-                    left: `${50 + delta * Math.sin(radian)}%`,
-                    top: `${50 + delta * Math.cos(radian)}%`
+                return <div onClick={bigger ? this.onClick(key) : undefined} className={`${cls.pointer} ${bigger ? cls.big : cls.small}`} key={key} style={{
+                    left: `${50 - delta * Math.sin(radian)}%`,
+                    top: `${50 + delta * Math.cos(radian)}%`,
+                    opacity: wisePosition >= key ? 1 : 0.6
                 }}>
                     {bigger && <div className={`${cls.small}`} />}
                 </div>
