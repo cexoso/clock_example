7f4b076d8db93dec8eb7ccef368a026ef7c7296d
cesoxiong <cesoxiong@tencent.com>
2019-08-01 15:11:09 +0800

    Updates

---
+++ precache-manifest.80c099c6a78b2007cac4e5ee648d546e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b32b163cf52c03a8dfeef38af403d5",
+    "url": "/clock_example/index.html"
+  },
+  {
+    "revision": "95245e694dbf57c5f30e",
+    "url": "/clock_example/static/css/main.0d86b965.chunk.css"
+  },
+  {
+    "revision": "664e01aae9878016e67c",
+    "url": "/clock_example/static/js/2.15b5d202.chunk.js"
+  },
+  {
+    "revision": "95245e694dbf57c5f30e",
+    "url": "/clock_example/static/js/main.04cd92be.chunk.js"
+  },
+  {
+    "revision": "a433bd185c875bc8be45",
+    "url": "/clock_example/static/js/runtime~main.e3a29a83.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8d5a57fc4133a0a9d92e7d0e75ee0c2.js"
+  "/clock_example/precache-manifest.80c099c6a78b2007cac4e5ee648d546e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clock_example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
