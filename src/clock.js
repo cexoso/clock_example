77eec6e65d369c441b0527177eb4d054e8c1bc32
cesoxiong <cesoxiong@tencent.com>
2019-08-01 16:53:38 +0800

    添加事件滚轮

--- src/clock.js
+++ src/clock.js
@@ -3,6 +3,24 @@ import Tick from "./tick"
 import cls from './clock.module.css'
 
 export default class Clock extends React.PureComponent {
+    componentDidMount() {
+        
+        const displaywheel = (e) => {
+            const evt = window.event || e
+            const delta = evt.detail ? evt.detail * (-120) : evt.wheelDelta
+            const { wisePosition } = this.state
+            this.setState({
+                wisePosition: Math.floor(Math.min(Math.abs(wisePosition - delta / 100), 72))
+            })
+        }
+
+        const mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel"
+
+        if (document.attachEvent)
+            document.attachEvent("on" + mousewheelevt, displaywheel)
+        else if (document.addEventListener)
+            document.addEventListener(mousewheelevt, displaywheel, false)
+    }
     state = {
         wisePosition: 0
     }
@@ -11,9 +29,10 @@ export default class Clock extends React.PureComponent {
     }
     render() {
         const { wisePosition } = this.state
+        console.log(wisePosition)
         return <div className={cls.container}>
             <Tick onClickBigger={this.onClickBigger} wisePosition={wisePosition} />
-            <div className={cls.clockWise} style={{transform: `rotate(${360 / 72 * wisePosition - 90}deg)`}} />
+            <div className={cls.clockWise} style={{ transform: `rotate(${360 / 72 * wisePosition - 90}deg)` }} />
             <div className={cls.center} />
         </div>
     }
