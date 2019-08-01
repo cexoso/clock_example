5bfb03d022e4b2f6d56f718347c71a1d4524c0fc
cesoxiong <cesoxiong@tencent.com>
2019-08-01 15:10:32 +0800

    Updates

---
+++ precache-manifest.f8d5a57fc4133a0a9d92e7d0e75ee0c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fe2d5b22f3023130405dc06f915e76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95245e694dbf57c5f30e",
+    "url": "/static/css/main.0d86b965.chunk.css"
+  },
+  {
+    "revision": "664e01aae9878016e67c",
+    "url": "/static/js/2.15b5d202.chunk.js"
+  },
+  {
+    "revision": "95245e694dbf57c5f30e",
+    "url": "/static/js/main.04cd92be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
